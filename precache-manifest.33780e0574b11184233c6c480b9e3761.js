28e6e3eb9840f03bd4c5e620c8ffa69536e46172
yessine chakroun <yessinechakroun@hotmail.com>
2020-02-19 11:44:47 +0100

    Updates

---
+++ precache-manifest.33780e0574b11184233c6c480b9e3761.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d649598c2cfeb0f9027653d3245893c",
+    "url": "/rest-api/index.html"
+  },
+  {
+    "revision": "a1ac703eda30f1765de9",
+    "url": "/rest-api/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "02d19dae4d9a3512f7c0",
+    "url": "/rest-api/static/js/2.5748bcfa.chunk.js"
+  },
+  {
+    "revision": "a1ac703eda30f1765de9",
+    "url": "/rest-api/static/js/main.68aeef53.chunk.js"
+  },
+  {
+    "revision": "14cb1fcbf770d38e91ee",
+    "url": "/rest-api/static/js/runtime-main.15b35c43.js"
+  }
+]);
